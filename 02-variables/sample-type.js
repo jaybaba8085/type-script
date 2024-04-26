47a9b621a30476eabe0784f431d7a1195037b023
jaybaba8085 <sharginjaypatel@gmail.com>
2024-04-26 23:08:59 +0530

    learning type script

---
+++ 02-variables/sample-type.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 88.6;
+var firstname = "Jay";
+var lastname = "Patel";
+console.log(found);
+console.log("Grade is  :" + grade);
+console.log("Name is   : " + firstname + " " + lastname);
+console.log("Found is ".concat(found, " and Name is ").concat(firstname, " ").concat(lastname));
---
+++ 02-variables/sample-type.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstname: string = "Jay";
+let lastname: string = "Patel";
+
+console.log(found);
+console.log("Grade is  :" + grade);
+console.log("Name is   : "+ firstname + " " + lastname);
+
+console.log(`Found is ${found} and Name is ${firstname} ${lastname}`);
