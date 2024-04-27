6de8335363da44a8e961f6fd1e9375f5d63222a2
jaybaba8085 <sharginjaypatel@gmail.com>
2024-04-27 14:02:36 +0530

    Access Modifier

--- 04-classes/Customer.js
+++ 04-classes/Customer.js
@@ -1,13 +1,69 @@
 var Customer = /** @class */ (function () {
-    function Customer(theFirst, theLast) {
+    function Customer(theFirst, theLast, age, gender) {
         this.firstName = theFirst;
         this.lastName = theLast;
+        this._age = age;
+        this._gender = gender;
     }
+    //Normal Getter and Setter Method
+    Customer.prototype.getFirstName = function () {
+        return this.firstName;
+    };
+    Customer.prototype.setFirstName = function (theFirstName) {
+        this.firstName = theFirstName;
+    };
+    Customer.prototype.getLastName = function () {
+        return this.lastName;
+    };
+    Customer.prototype.setLastName = function (theLastName) {
+        this.lastName = theLastName;
+    };
+    Object.defineProperty(Customer.prototype, "age", {
+        //Other Ways To Implement Getter And Setter in Type Script
+        //This Set Accessor Does Not Required to have Any Return Type
+        get: function () {
+            return this._age;
+        },
+        set: function (value) {
+            if (value < 0 || value > 120) {
+                console.log("Invalid Age");
+            }
+            else {
+                this._age = value;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "gender", {
+        get: function () {
+            return this._gender;
+        },
+        set: function (value) {
+            if (!(value === "Male" || value === "Female")) {
+                throw new Error("Gender must be Male or Female.");
+            }
+            else {
+                this._gender = value;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Customer;
 }());
-var myCustomer = new Customer("Jay", "Patel");
+var myCustomer = new Customer("Jay", "Patel", 23, "Male");
+// Direct Changeing the Name
 // myCustomer.firstName = "J";
 // myCustomer.lastName = "P";
+// Without Getter and setter 
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
-;
+myCustomer.setFirstName("Sam");
+myCustomer.setLastName("will");
+// With Getter and Setter
+console.log(myCustomer.getFirstName());
+console.log(myCustomer.getLastName());
+//Wuth The Help of Accessors
+console.log(myCustomer.age);
+console.log(myCustomer.gender);
--- 04-classes/Customer.ts
+++ 04-classes/Customer.ts
@@ -1,18 +1,78 @@
 class Customer {
     private firstName: string;
     private lastName: string;
+    private _age: number;
+    private _gender: string;
 
-    constructor(theFirst: string, theLast: string) {
+    constructor(theFirst: string, theLast: string, age: number, gender: string) {
         this.firstName = theFirst;
         this.lastName = theLast;
+        this._age = age;
+        this._gender = gender;
+    }
+
+
+    //Normal Getter and Setter Method
+
+    public getFirstName(): string {
+        return this.firstName;
+    }
+    public setFirstName(theFirstName: string): void {
+        this.firstName = theFirstName;
+    }
+    public getLastName(): string {
+        return this.lastName;
+    }
+    public setLastName(theLastName: string): void {
+        this.lastName = theLastName;
+    }
+
+    //Other Ways To Implement Getter And Setter in Type Script
+    //This Set Accessor Does Not Required to have Any Return Type
+
+    public get age(): number {
+        return this._age;
+    }
+
+    public set age(value: number) {
+        if (value < 0 || value > 120) {
+            console.log("Invalid Age");
+        } else {
+            this._age = value;
+        }
+    }
+    
+    public get gender(): string {
+        return this._gender;
+    }
+    
+    public set gender(value: string) {
+        if (!(value === "Male" || value === "Female")) {
+            throw new Error("Gender must be Male or Female.");
+        } else {
+            this._gender = value;
+        }
     }
 }
 
-let myCustomer = new Customer("Jay", "Patel");
+let myCustomer = new Customer("Jay", "Patel", 23, "Male");
 
+// Direct Changeing the Name
 // myCustomer.firstName = "J";
 // myCustomer.lastName = "P";
 
+// Without Getter and setter 
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
-;
+
+myCustomer.setFirstName("Sam");
+myCustomer.setLastName("will");
+
+// With Getter and Setter
+console.log(myCustomer.getFirstName());
+console.log(myCustomer.getLastName());
+
+//Wuth The Help of Accessors
+console.log(myCustomer.age);
+console.log(myCustomer.gender);
+
