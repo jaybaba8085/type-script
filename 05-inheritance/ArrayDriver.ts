e9b52ecf080ab4821767ac6f1bc1939ad6215b21
jaybaba8085 <sharginjaypatel@gmail.com>
2024-05-25 17:28:08 +0530

    updates

--- 05-inheritance/ArrayDriver.ts
+++ 05-inheritance/ArrayDriver.ts
@@ -7,11 +7,11 @@ let myCircle = new Circle(0, 0, 5);
 let myRectangle = new Rectangle(0, 0, 1, 2);
 
 //Declare an Array of shapes ...initially empty
-let theShapes: Shape[] =[];
+let theShapes: Shape[] = [];
 theShapes.push(myShape);
 theShapes.push(myCircle);
 theShapes.push(myRectangle);
 
-for(let tempShape of theShapes){
+for (let tempShape of theShapes) {
     console.log(tempShape.getInfo());
 }
